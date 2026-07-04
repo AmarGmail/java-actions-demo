f6b0539552d431604b6ddda780aca6a9d690bb52
Amarjyoti Lahkar <amarjyoti.lahkar@gmail.com>
2026-07-04 19:20:18 +0530

    Delete HJelloWorld.java

--- HJelloWorld.java
+++
@@ -1,8 +0,0 @@
-// A simple Java program to print "Hello, World!" to the console
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        // Print the message to the console
-        System.out.println("Hello, World! from github");
-    }
-}
